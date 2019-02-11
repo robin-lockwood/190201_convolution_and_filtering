db71654ea1a9842fc2098b47f40025d337e1b2f8
Robin Lockwood <robin.lockwood@umontana.edu>
2019-02-11 00:27:56 -0700

    Added comments and removed extraneous print statements.

--- edge_test.py
+++ edge_test.py
@@ -1,8 +1,9 @@
 import numpy as np
 from random import randint
 
-# Fills 'empty' cells with fill value. r is the radius of the kernel
-def gFill(u, v, r, fill, img):
+
+# Fills 'empty' cells with fill value and r is the radius of the kernel
+def g_fill(u, v, r, fill, img):
 
     if u < r or v < r or u > img.shape[0]-r-1 or v > img.shape[1]-r-1:
         return fill
@@ -11,17 +12,17 @@ def gFill(u, v, r, fill, img):
 
 
 # Fills empty cells with values mirrored
-def gMirror(u, v, r, img):
+def g_mirror(u, v, r, img):
     j = u
     k = v
     if u < r:
-        c=2*(r-u)-1
-        print(c)
+        # the distance is doubled from the r-axis
+        c = 2*(r-u)-1
         j = u+c
     if u > img.shape[0]-r-1:
-        j = img.shape[0]-r-(u-(img.shape[0]-r-1))
+        j = (img.shape[0]-r)-(u-(img.shape[0]-r-1))
     if v < r:
-        c=2*(r-v)-1
+        c = 2*(r-v)-1
         k = v+c
     if v > img.shape[1]-r-1:
         # take radius from end, take from that the distance from v
@@ -31,7 +32,7 @@ def gMirror(u, v, r, img):
 
 
 # Fills empty cells with the nearest cell
-def gNN(u, v, r, img):
+def g_nn(u, v, r, img):
     if u < r:
         u = r
     if u > img.shape[0]-r-1:
@@ -40,37 +41,40 @@ def gNN(u, v, r, img):
     if v < r:
         v = r
     if v > img.shape[1]-r-1:
-        print(u,v)
         v = img.shape[1] - r-1
-        print(u,v)
 
     return img[u, v]
 
-radius=3
 
-kernel=[[1,1,1],[1,1,1],[1,1,1]]
-image = np.zeros((10, 8))
+# Corresponds to kernel radius
+radius = 4
+
+# This image has extra rows and columns
+image = np.zeros((20, 16))
 
 # fill image with data
-for i in range(radius, image.shape[0]-radius):
-    for j in range(radius, image.shape[1]-radius):
-        image[i, j] = randint(0, 10)
+for n in range(radius, image.shape[0]-radius):
+    for o in range(radius, image.shape[1]-radius):
+        image[n, o] = randint(0, 10)
 
 # create array for storing new values
-lg_image = np.zeros((10, 8))
+lg_image = np.zeros((20, 16))
 
-# for i in range(0,image.shape[0]):
-#     for m in range(0,image.shape[1]):
-#                 lg_image[i][m] = gFill(i, m, 2, 12, image)
-
+#################################
+# Test these with different radii
+###################################
 # for i in range(0, image.shape[0]):
 #     for m in range(0, image.shape[1]):
-#         lg_image[i][m] = gMirror(i, m, radius, image)
-
+#                 lg_image[i][m] = g_fill(i, m, 2, 12, image)
+#
 for i in range(0, image.shape[0]):
     for m in range(0, image.shape[1]):
-        lg_image[i][m] = gNN(i, m, radius, image)
+        lg_image[i][m] = g_mirror(i, m, radius, image)
+#
+# for i in range(0, image.shape[0]):
+#     for m in range(0, image.shape[1]):
+#         lg_image[i][m] = g_nn(i, m, radius, image)
 
+# print to verify
 for i in lg_image:
     print(i)
-
