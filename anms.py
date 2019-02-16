a81900ca49538fa65d76c5a69ec03d826afa8253
Robin Lockwood <robin.lockwood@umontana.edu>
2019-02-15 23:14:39 -0700

    code for adaptive non-maximal suppression. Not perfect but it works.

---
+++ anms.py
@@ -0,0 +1,167 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from math import exp
+from math import sqrt
+
+
+# Read image
+# img_color = plt.imread('noisy_big_chief.jpeg')
+# img_color = plt.imread('image_1.jpg')
+# I_gray = img_color.mean(axis=2)
+#
+# # Output original grayscale image
+# fig = plt.figure(figsize=(18, 16), dpi=80, facecolor='w', edgecolor='k')
+# ps = plt.imshow(I_gray, cmap='gray')
+# plt.show()
+
+
+# There are more elegant ways we could do this - but this works
+# g(u,v) represents the sum of all pixels around u,v
+def gdef(u, v, img):
+    sum = 0.0
+    sum += img[u - 1, v - 1] + img[u - 1, v] + img[u - 1, v + 1]
+    sum += img[u, v - 1] + img[u, v] + img[u, v + 1]
+    sum += img[u + 1, v - 1] + img[u + 1, v] + img[u + 1, v + 1]
+
+    return sum
+
+
+# h(u,v) represents any h(j,k)
+def hdef(u, v, img, sigma):
+    return 1.0 / 9.0
+
+
+def convolve(g, h, img):
+    out = np.zeros((img.shape[0] - 2, img.shape[1] - 2))  # Output image has 2 less rows and columns from convolution
+
+    # Simply loop through all pixels in the image and apply g and h to those pixels
+    for u in range(1, out.shape[0] + 1):
+        for v in range(1, out.shape[1] + 1):
+            out[u - 1, v - 1] = g(u, v, img) * h(u, v, img, 0.0)
+
+    return out
+
+
+# Our h gauss function
+def hgauss(j, k, img, rad, sigma):
+    val = j * j + k * k
+    val = val / (2 * sigma * sigma)
+    val = exp(-val)
+
+    return val * 1 / ((2 * rad + 1) ^ 2)
+
+
+# A generic G function
+def gn(u, v, img):
+    return img[u, v]
+    return 0.0
+
+
+# A generic convolution function
+def convolveGauss(g, h, img, rad, sigma):
+    out = np.zeros((img.shape[0] - rad - rad,
+                    img.shape[1] - rad - rad))  # Output image has 2 less rows and columns from convolution
+
+    ja = list(range(-rad, rad + 1))
+    ka = list(range(-rad, rad + 1))
+
+    # Simply loop through all pixels in the image and apply g and h to those pixels
+    for u in range(rad, img.shape[0] - rad - rad):
+        for v in range(rad, img.shape[1] - rad - rad):
+            sum = 0.0
+
+            for j in ja:
+                for k in ka:
+                    sum += g(u + j, v + k, img) * h(j, k, img, rad, sigma)
+
+            out[u - rad, v - rad] = sum
+
+    return out
+
+
+def hsobel(j, k, img, rad, sigma):
+    matr = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    j = j + 1
+    k = k + 1
+    return matr[j, k]
+
+
+def hsobelT(j, k, img, rad, sigma):
+    matr = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    matr = np.transpose(matr)
+    j = j + 1
+    k = k + 1
+    return matr[j, k]
+
+
+def Harris(img, radius, sd):
+    Iu = convolveGauss(gn, hsobel, img, 1, 1)
+    Iv = convolveGauss(gn, hsobelT, img, 1, 1)
+
+    Iuu = np.multiply(Iu, Iu)
+    Ivv = np.multiply(Iv, Iv)
+    Iuv = np.multiply(Iu, Iv)
+
+    Iuu = convolveGauss(gn, hgauss, Iuu, radius, sd)
+    Ivv = convolveGauss(gn, hgauss, Ivv, radius, sd)
+    Iuv = convolveGauss(gn, hgauss, Iuv, radius, sd)
+
+    return np.multiply(Iuu, Ivv) - np.multiply(Iuv, Iuv)
+
+
+def get_high_intensity(img, val):
+    p = []
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i, j] > val:
+                p.append(((i, j), img[i, j]))
+    return p
+
+
+def distance(p1, p2):
+    return sqrt(pow((p1[0] - p2[0]), 2) + pow((p1[1] - p2[1]), 2))
+
+
+# adaptive non-maximal suppression
+# points describes a matrix of tuples for storing (x,y) and intensity and n is the number of top radii to pick from
+def anms(points, n=500, t=0.9):
+    s = []
+    for i in range(len(points)):
+        radius = np.Infinity
+        p = points[i]
+        for j in range(len(points)):
+            pp = points[j]
+            # for every point skip if same pixel otherwise check if p has more or less intensity
+            if (p[0][0] != pp[0][0] and p[0][1] != pp[0][1]) and (p[1] < t * pp[1]):
+                d = distance(p[0], pp[0])
+                if d < radius:
+                    radius = d
+        s.append(((p[0][0], p[0][1]), radius))
+    s.sort(key=lambda l: l[1], reverse=True)
+    # return a matrix of the top n points
+    return s[0:n]
+
+
+img_color = plt.imread('chessboard.png')
+# I_gray = img_color.mean(axis=2)
+cimg = Harris(img_color, 1, 2.0)
+
+fig = plt.figure(figsize=(18, 16), dpi=80, facecolor='w', edgecolor='k')
+ps = plt.imshow(cimg, cmap='gray')
+plt.show()
+
+hih_points = get_high_intensity(cimg, 1.0)
+descriptors = anms(hih_points, 100, 0.95)
+filtered_image = np.zeros(cimg.shape)
+
+for x in range(cimg.shape[0]):
+    for y in range(cimg.shape[1]):
+        for z in descriptors:
+            if z[0][0] == x and z[0][1] == y:
+                filtered_image[x, y] = 1
+                
+fig = plt.figure(figsize=(18, 16), dpi=80, facecolor='w', edgecolor='k')
+ps = plt.imshow(filtered_image, cmap='gray')
+plt.show()
